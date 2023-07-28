f022252f690bf9d9744747b4f4d6f384db8b04ac
EsikAntony <esik.antony@gmail.com>
2023-07-28 11:22:33 +0300

    [1] Rewrites readme.md and adds component design sheet

--- camunda-task-dispatcher-runtime/src/main/java/com/ae/camunda/dispatcher/TaskDispatcherApplication.java
+++ camunda-task-dispatcher-runtime/src/main/java/com/ae/camunda/dispatcher/TaskDispatcherApplication.java
@@ -21,7 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskDispatcherApplication {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         SpringApplication.run(TaskDispatcherApplication.class, args);
     }
 }
